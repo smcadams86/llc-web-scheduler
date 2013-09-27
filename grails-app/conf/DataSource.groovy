fde9eee6ad44058e85b3ba8477aace858ab3c586
Steven McAdams <smcadams86@gmail.com>
2013-09-27 10:11:00 -0700

    log sql, use java 1.7, use cache.provider_class='com.opensymphony.oscache.hibernate.OSCacheProvider'

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -7,7 +7,7 @@ dataSource {
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
-//    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
+    //    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
     cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
 }
 
@@ -27,15 +27,18 @@ environments {
     }
     production {
         dataSource {
-		dbCreate = "update"
-		driverClassName = "org.postgresql.Driver"
-		dialect = org.hibernate.dialect.PostgreSQLDialect
-    
-		uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
-
-		url = "jdbc:postgresql://"+uri.host+uri.path
-		username = uri.userInfo.split(":")[0]
-		password = uri.userInfo.split(":")[1]
+            dbCreate = "update"
+            driverClassName = "org.postgresql.Driver"
+            dialect = org.hibernate.dialect.PostgreSQLDialect
+            uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
+            url = "jdbc:postgresql://"+uri.host+uri.path
+            username = uri.userInfo.split(":")[0]
+            password = uri.userInfo.split(":")[1]
+            
+            logSql = true
+        }
+        hibernate {
+            cache.provider_class='com.opensymphony.oscache.hibernate.OSCacheProvider'
         }
     }
 }
